e6acbde96d7230c33872ec177faca7c9cdb8cbed
Egor Pobylets <egor.pobylets@cogniteq.com>
2023-06-28 07:24:48 +0200

    feat: add functioanality of saving pictures

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-import express from 'express'
-import ws from 'express-ws'
-import cors from 'cors'
-import * as fs from 'fs'
-import * as path from 'path'
+
+
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
-
-const WSserver = ws(app)
-const aWss = WSserver.getWss()
-
+const WSServer = require('express-ws')(app)
 const PORT = process.env.PORT || 8800
+const aWss = WSServer.getWss()
 
 app.use(cors())
 app.use(express.json())
 
-app.ws('/', (ws, req) => {
-  console.log('SUCCESS CONNETION')
-  ws.send('success conect')
-
-  ws.on('message', msg => {
-    msg = JSON.parse(msg)
-    switch(msg.method) {
-      case 'connection':
-        connectionHandler(ws, msg)
-        break;
-      case 'draw':
-        broadcastConnection(ws, msg)
-    }
-  })
+app.ws('/', (ws) => {
+    ws.on('message', (msg) => {
+        msg = JSON.parse(msg)
+        switch (msg.method) {
+            case "connection":
+                connectionHandler(ws, msg)
+                break
+            case "draw":
+                broadcastConnection(ws, msg)
+                break
+        }
+    })
 })
 
 app.post('/image', (req, res) => {
-  try {
-    const data = req.body.img.replace('...', '')
-    fs.writeFileSync(path.resolve('__dirname', 'files', `${req.query.id}.jpg`, data, 'base64'))
+    try {
+        const data = req.body.img.replace(`data:image/png;base64,`, '')
+        fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
 
-    return res.status(200).json({ message: 'Saved' })
-  } catch (e) {
-    console.log(e)
-    return res.status(500).json('Server error')
-  }
+        return res.status(200).json({message: "Saved"})
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('error')
+    }
 })
+
 app.get('/image', (req, res) => {
-  try {
-    const file = fs.readFileSync(path.resolve('__dirname', 'files', `${req.query.id}.jpg`))
-    const data = '...' + file.toString('base64')
+    try {
+        const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
+        const data = `data:image/png;base64,` + file.toString('base64')
 
-    res.json(data)
-  } catch (e) {
-    console.log(e)
-    return res.status(500).json('Server error')
-  }
+        res.json(data)
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('error')
+    }
 })
-
-app.listen(PORT, () => console.log(`SERVER WORK POTS - ${PORT}`))
 
 const connectionHandler = (ws, msg) => {
-  ws.id = msg.id
-  broadcastConnection(ws, msg)
+    ws.id = msg.id
+
+    broadcastConnection(ws, msg)
 }
 
-const broadcastConnection = (ws, msg) => {
-  aWss.clients.forEach(client => {
-    if (client.id === msg.id) {
-      client.send(JSON.stringify(msg))
-    }
-  })
+const broadcastConnection = (_, msg) => {
+    aWss.clients.forEach(client => {
+        if (client.id === msg.id) {
+            client.send(JSON.stringify(msg))
+        }
+    })
 }
+
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
